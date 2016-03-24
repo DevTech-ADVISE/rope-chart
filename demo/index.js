cf1426e91c2cb12459026f85f97cf207ec7149c6
Jack Compton <jack.compton@gmail.com>
2016-03-24 13:53:12 -0400

    trivial whitespace changes in demo

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,11 @@
 var myRopeChart = RopeChart('div#myRopeChart');
-var data = [{name:'Bill',value:'33'},{name:'Bob',value:'54'},{name:'Janet',value:'21'},{name:'Phil',value:'60'},{name:'James',value:'29'},{name:'Annie',value:'36'},{name:'Eloise',value:'44'}];
+var data = [{name:'Bill',value:'33'},
+            {name:'Bob',value:'54'},
+            {name:'Janet',value:'21'},
+            {name:'Phil',value:'60'},
+            {name:'James',value:'29'},
+            {name:'Annie',value:'36'},
+            {name:'Eloise',value:'44'}];
 
 myRopeChart
   .knotRadius(5)
