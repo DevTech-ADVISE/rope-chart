54a4419e254eb2254a4796ec7c71853608b94ebd
Andrew Blowe <tehandyb@gmail.com>
2016-09-06 22:57:59 -0400

    Updates

--- index.js
+++ index.js
@@ -5,48 +5,59 @@ var james = RopeChart('div#james');
 var annie = RopeChart('div#annie');
 
 var data = [
-  {name:'Average Joe',value:'40'},
-  {name:'Bob',value:'38'},
-  {name:'Janet',value:'21'},
-  {name:'Phil',value:'60'},
-  {name:'James',value:'22'},
-  {name:'Annie',value: '57'},
-  {name:'Eloise',value:'41'}
+  {name:'Average Joe',value:'100'},
+  {name:'Bob',value:'50'},
+  {name:'Janet',value:'50'},
+  {name:'Phil',value:'50'},
+  {name:'James',value:'50'},
+  {name:'Annie',value: '50'},
+  {name:'Eloise',value:'50'},
+  {name: 'Bill', value: '52'}
 ];
 
 myRopeChart
   .knotRadius(10)
   .ropeWidth(4)
   .showThreshold(true)
-  .focusName('Bob')
-  .tooltipContent(function(d) { return "Custom markup showing <em>" + d.label + "</em> score is <em>" + d.value + "</em>"; })
+  .focusName('Bill')
+  .tooltipContent({ threshold: function(d) { return "Custom markup showing <em>" + d.label + "</em> score is <em>" + d.value + "</em>"; } })
   .tooltipLabel('&#128129;')
+  .showTooltip({ threshold: true, top: true})
+  .marginLeftPercentage(25)
   .render(data);
 
 averageJoe
   .knotRadius(10)
   .ropeWidth(4)
   .showThreshold(true)
+  .showTooltip({ threshold: true })
   .focusName('Average Joe')
+  .marginLeftPercentage(25)
   .render(data);
 
 amazingPhil
  .knotRadius(10)
  .ropeWidth(4)
  .showThreshold(true)
+ .showTooltip({ threshold: true, top: true })
  .focusName('Phil')
+ .marginLeftPercentage(25)
  .render(data);
 
 james
  .knotRadius(10)
  .ropeWidth(4)
  .showThreshold(true)
+ .showTooltip({ threshold: true, top: true })
  .focusName('James')
+ .marginLeftPercentage(25)
  .render(data);
 
 annie
  .knotRadius(10)
  .ropeWidth(4)
  .showThreshold(true)
+ .showTooltip({ threshold: true })
  .focusName('Annie')
+ .marginLeftPercentage(25)
  .render(data);
