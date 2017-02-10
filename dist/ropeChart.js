311e594e488e2d68b716598f72fcd8fa091a0a34
Andrew Blowe <tehandyb@gmail.com>
2017-02-10 15:21:38 -0500

    cleaned up rank logic

--- dist/ropeChart.js
+++ dist/ropeChart.js
@@ -1448,22 +1448,18 @@ return /******/ (function(modules) { // webpackBootstrap
 	  if (ascending) sortedValues.reverse();
 
 	  var indexOfValue = sortedValues.indexOf(value);
-	  var rankArray = [];
-	  for (var i = 0; i < sortedValues.length; i++) {
-	    if (sortedValues[i - 1]) {
-	      // If the previous value equals the current value, the current value is tied/set to the same rank as the previous value
-	      if (sortedValues[i - 1] === sortedValues[i]) {
-	        rankArray.push(rankArray[i - 1]);
-	      } else {
-	        rankArray.push(i + 1);
-	      }
-	    } else {
-	      // Start with rank 1 for the first element
-	      rankArray.push(1);
-	    }
+	  var rankArray = [1]; // Start with rank 1 for the first element
+	  for (var i = 1; i < sortedValues.length; i++) {
 	    // If the rank array has gone far enough to rank the selected value then break out and return the rank
 	    if (rankArray.length == indexOfValue + 1) {
 	      return ordinal(rankArray[indexOfValue]);
+	    }
+
+	    // If the previous value equals the current value, the current value is tied/set to the same rank as the previous value
+	    if (sortedValues[i - 1] === sortedValues[i]) {
+	      rankArray.push(rankArray[i - 1]);
+	    } else {
+	      rankArray.push(i + 1);
 	    }
 	  }
 	};
